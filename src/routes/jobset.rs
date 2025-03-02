60db70c39b1fa401742c9cb06bf047bca3e20a4b
DestinyofYeet <ole@ole.blue>
2025-03-02 23:46:21 +0100

    feat(jobsets): Starts work on jobsets

--- src/routes/jobset.rs
+++ src/routes/jobset.rs
@@ -1,8 +1,14 @@
 use leptos::prelude::*;
+use leptos_router::hooks::use_params_map;
 
 #[component]
 pub fn Jobset() -> impl IntoView {
+    let params = use_params_map();
+
+    let project_id = params.read_untracked().get("proj-id").unwrap_or_default();
+    let jobset_id = params.read_untracked().get("jobset-id").unwrap_or_default();
+
     view! {
-        <p>"Hello from Jobset"</p>
+        <p>"Hello from Jobset " {jobset_id} " on project " {project_id}</p>
     }
 }
