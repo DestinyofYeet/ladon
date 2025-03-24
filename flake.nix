bde1489dd692e6b743bd8b7a86c3a53d258f6506
DestinyofYeet <olebend@gmail.com>
2025-03-24 21:10:27 +0100

    feat(meta/flake.nix): Attempts to start stylance with direnv

--- flake.nix
+++ flake.nix
@@ -26,6 +26,19 @@
 
         PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
         DATABASE_URL = "sqlite:tmp/db.sqlite";
+
+        # shellHook = ''
+        #   if [ -f ".direnv/stylance_pid" ]; then
+        #     pid=$(cat ".direnv/stylance_pid")
+        #     if ps -p $pid > /dev/null
+        #     then
+        #       echo "Stylance is running: $pid"
+        #     else
+        #       _=$(stylance -w ./ & echo $! > ".direnv/stylance_pid")&
+        #   else
+        #     _=$(stylance -w ./ & echo $! > ".direnv/stylance_pid")&
+        #   fi
+        # '';
       };
 
       packages.x86_64-linux.default = pkgs.callPackage ./pkg.nix { };
